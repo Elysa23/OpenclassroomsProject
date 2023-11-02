fcd441cafd0420f2f2198a38e3d25761df995ff7
Elysa23 <121961510+Elysa23@users.noreply.github.com>
2023-11-02 14:42:09 +0100

    Add files via upload

---
+++ premierpas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+	ejfefefjefjeeeejejo
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); //OUAUIAIAI 
+?>.</p>
+		
+		<!--C'est seulement maintenant que ça marche enfin!!
+		
+		//C'est seulement maintenant que ça marche enfin!!
+		/*FKEKFEZ/*
+		
+</body>
+</html>
